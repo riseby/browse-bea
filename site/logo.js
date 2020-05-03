74b304546b811d7f7e4821214670892cf593017c
Emil <emil.riseby@gmail.com>
2020-05-03 15:43:33 +0200

    movement to letter B in logo and minor adj.

--- site/logo.js
+++ site/logo.js
@@ -1,24 +1,28 @@
 let xySize = 100
 let maxRadius = xySize/2
-let minRadius = 35
+let minRadius = 30
 let noiseWeight = maxRadius - minRadius
 let n = 0.0
 let nincrement = 0.01
 
 var c1noB = 'black'
 var c2noB = 'white'
+var c3noB = c1noB
 var c1B = [255, 193, 7]
 var c2B = [33, 37, 41]
+var c3B = '#dee2e6'
 
 var c1 = c1noB
 var c2 = c2noB
+var c3 = c3noB
 
 function setup() {
   var canvas = createCanvas(xySize, xySize)
   canvas.parent('logo')
   frameRate(30)
-  textSize(50);
-  textAlign(CENTER, CENTER);
+  textSize(60);
+  textFont('Courier Prime')
+  textAlign(CENTER, CENTER)
   smooth()
 }
 
@@ -30,32 +34,52 @@ function draw() {
   //let w = color('gray')
 
   fill(fc)
-  //stroke(sc)
-  noStroke()
-  strokeWeight(1.3)
+  stroke(color(c3))
+  //noStroke()
+  strokeWeight(1)
   translate(height/2,width/2)
   n += nincrement
+  var dxMax = 0.0;
+  var dyMax = 0.0;
+  var magMax = 0.0;
+
   beginShape()
     for(var i = 0;i < TWO_PI;i += 0.1){
       var x = cos(i)
       var y = sin(i)
       var r = noise(x+10,y+10,n)
-      var dx = cos(i)*(r*noiseWeight+minRadius)
-      var dy = sin(i)*(r*noiseWeight+minRadius)
-      vertex(dx,dy)
+      vertex(x*(r*noiseWeight+minRadius),y*(r*noiseWeight+minRadius))
     }
+
   endShape(CLOSE)
+  stroke(color('red'))
+  strokeWeight(3)
+  var dxTot = 0.0;
+  var dyTot = 0.0;
+  for(var i = 0;i < 8;i += 1){
+    var x = cos(i*PI/4)
+    var y = sin(i*PI/4)
+    var r = noise(x+10,y+10,n)
+    dxTot += (x * (r*noiseWeight+minRadius))
+    dyTot += (y * (r*noiseWeight+minRadius))
+
+  }
+
+  //point(dxTot, dyTot)
   noStroke()
   fill(sc)
-  text("B", 0, (textAscent()- textDescent())/4)
+  text("B", dxTot, dyTot + (textAscent()- textDescent())/4)
+  //text("B", dxTot, dyTot)
 }
 
 function setBeaLogoColors(b) {
   if(b){
     c1 = c1B
     c2 = c2B
+    c3 = c3B
   } else {
     c1 = c1noB
     c2 = c2noB
+    c3 = c3noB
   }
 }
